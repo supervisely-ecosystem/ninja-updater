7e9d740ec145a26d3bd50e79c8d8bed73c57631f
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-06-16 12:17:16 +0300

    Removed dtools updater.

--- src/ui/info.py
+++ src/ui/info.py
@@ -1,6 +1,4 @@
-import subprocess
-
-from supervisely.app.widgets import Text, Card, Container, Button
+from supervisely.app.widgets import Text, Card, Container
 import supervisely as sly
 
 import src.globals as g
@@ -8,14 +6,12 @@ import src.globals as g
 ssh_text = Text()
 ssh_text.hide()
 
-update_dtools_button = Button("Update dtools", icon="zmdi zmdi-refresh")
 
 card = Card(
     title="1️⃣ Info",
     description="Check that everything is set up correctly.",
     collapsable=True,
     content=Container(widgets=[ssh_text]),
-    content_top_right=update_dtools_button,
 )
 
 if g.AppState.ssh_status:
@@ -28,32 +24,3 @@ else:
     ssh_text.status = "error"
 
 ssh_text.show()
-
-
-@update_dtools_button.click
-def update_dtools():
-    update_dtools_button.text = "Updating..."
-
-    return_code = subprocess.check_call(
-        "pip3 install git+https://github.com/supervisely/dataset-tools",
-        shell=True,
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-
-    if return_code != 0:
-        sly.logger.error(f"Failed to update dtools. Return code: {return_code}")
-
-        sly.app.show_dialog(
-            title="Update dtools failed",
-            description=(
-                f"Updated of dtools failed with return code {return_code}. "
-                "It's recommended to restart the app."
-            ),
-            status="error",
-        )
-
-    else:
-        sly.logger.info("dtools updated successfully.")
-
-    update_dtools_button.text = "Update dtools"
